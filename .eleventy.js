836b67bb8955e4e3533c3549546aa93f58727217
Dan Canetti <info@danielcanetti.com>
2020-05-12 08:51:01 +0100

    Removing wallet. from meta.

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,6 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addShortcode('monetize11ty', function (providerurl, username, enable) {
-        return `<meta name="monetization" content="wallet.${providerurl}/${username}">
+        return `<meta name="monetization" content="${providerurl}/${username}">
             {% if ${enable} == "true" %}
                 Testing
             {% endif %}`
