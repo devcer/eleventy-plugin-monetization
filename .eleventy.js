c7d9555887c52f177e92175bde63c20a108ce2aa
Dan Canetti <info@danielcanetti.com>
2020-05-12 01:43:12 +0100

    Updates to .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,4 @@
+eleventyConfig.walletLink = 'walletlink'
 eleventyConfig.addShortcode('monetize11ty', function (providerurl, username) {
-    return `<meta name="monetization" content="$wallet.${providerurl}/${username}">`
+    return `<meta name="monetization" content="$wallet.${eleventyConfig.walletlink}">`
 })
