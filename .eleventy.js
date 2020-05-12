6320d1d0ae157cc6dc1d4d03632c3dc10d5f219f
Dan Canetti <info@danielcanetti.com>
2020-05-12 08:59:48 +0100

    Working on it

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,8 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addShortcode('monetize11ty', function (providerurl, username, enable) {
         return `<meta name="monetization" content="${providerurl}/${username}">
-            {% if ${enable} == "true" %}
-                Testing
-            {% endif %}`
+        {{{#if ${enable}}}}
+            Testing
+        {{{/if}}}`
     })
 };
