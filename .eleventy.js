e78b66325e5707299b18ce80a9d232f085ccef55
Dan Canetti <info@danielcanetti.com>
2020-05-24 22:24:41 +0100

    Fix issue: monetization.js file blocked by Ublock

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,13 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy({
         "node_modules/eleventy-plugin-monetization/dist/monetization.css": "assets/monetization.css",
-        "node_modules/eleventy-plugin-monetization/dist/monetization.js": "assets/monetization.js"
+        "node_modules/eleventy-plugin-monetization/dist/monetization.js": "assets/monet.js"
     });
     eleventyConfig.addShortcode('monetize11ty', function (providerurl, username) {
         return `
             <meta name="monetization" content="${providerurl}/${username}">
             <link rel="stylesheet" href="/assets/monetization.css">
-            <script type="text/javascript" defer src="/assets/monetization.js"></script>
+            <script type="text/javascript" defer src="/assets/monet.js"></script>
         `
     });
 };
